912dc6aa05cacaa458078318ddf2a5cf530f7378
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-07-11 10:30:53 -0500

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_06_182908) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_06_184550) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
